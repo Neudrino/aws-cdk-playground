48bba353a42d76a7a8cc4a3fa665af05aced5468
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-11-28 17:05:54 +0100

    add EC2 instance
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- lib/aws-cdk-playground-stack.ts
+++ lib/aws-cdk-playground-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { Vpc, SubnetType, Instance, AmazonLinuxImage, InstanceType } from 'aws-cdk-lib/aws-ec2';
 
 export class AwsCdkPlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,13 +15,19 @@ export class AwsCdkPlaygroundStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    const vpc = new Vpc(this, 'MainVpc', {
+    const vpc = new Vpc(this, 'lehnertf-delete-me-vpc', {
       maxAzs: 2,
       subnetConfiguration: [{
         cidrMask: 24,
-        name: 'public-subnet',
+        name: 'lehnertf-public-subnet',
         subnetType: SubnetType.PUBLIC,
       },]
+    })
+
+    const ec2 = new Instance(this, 'lehnertf-delete-me-ec2', {
+      vpc,
+      instanceType: new InstanceType('t2.micro'),
+      machineImage: new AmazonLinuxImage(),
     })
 
   }
